e9ee59ad3bf889bd4380769000eec12dd9b981f9
Torsten Bisanz-Blank <torsten.bisanz-blank@students.coding-bootcamps.eu>
2021-06-16 15:26:10 +0200

    Torsten Button

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <button>hier zu drücken bringt gar nichts --> Gruss Torsten</button>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<script></script>
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
-</script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
