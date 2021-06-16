83996b61fae13190191626244c3eb97f442451b0
Nicolai Simon <nicolai.simon@students.coding-bootcamps.eu>
2021-06-16 15:52:53 +0200

    Input component for test

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Input />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Input from "./components/Input.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Input,
   },
 };
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    alt="Nico"
+    src="https://db3pap007files.storage.live.com/y4mgUmgXvipNonAedQ5RCBgLQaULpbnXaBJKJc_FqK7-UuozEJnUPontCkOnxhkVwxGUW7sYrrkz_LlGfcxbcUaSC-LEJgiAichEYiJ6vXMqK6zgGUso_3GKiyiW7MelEr-fznB-Xf1N5eB3xUxvmDDGWJst7QAJZ_kxLnhfem-LNlo2FoQyKj0p1bsJRi0ffZF?width=540&height=540&cropmode=none"
+    width="540"
+    height="540"
+  />
+  <input type="text" />
+  <input type="text" />
+  <input type="button" value="GO!" />
+</template>
+
+<script>
+export default {
+  name: "InputComponent",
+};
+</script>
