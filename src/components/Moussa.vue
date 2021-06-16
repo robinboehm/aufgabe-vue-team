425c55746bd9b9d86b79d2fb13240714313d742d
Moussa Tougri <moussa.tougri@students.coding-bootcamps.eu>
2021-06-16 15:52:40 +0200

    first commit

---
+++ src/components/Moussa.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>Hello there!</h1>
+  <button @click="OnClick">Hallo</button>
+</template>
+
+<script>
+export default {
+  name: "Moussa",
+
+  data() {
+    return {
+      hallo: "Hallo zusammen",
+    };
+  },
+
+  methods: {
+    OnClick() {
+      console.log(this.hallo);
+    },
+  },
+};
+</script>
