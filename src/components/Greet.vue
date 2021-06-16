287ba7f02da2279a7ff93ef2ce8a1c09c3c2792a
Jan Gawarecki <jan.gawarecki@students.coding-bootcamps.eu>
2021-06-16 15:54:46 +0200

    Add Greet.vue component

---
+++ src/components/Greet.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2>{{ msg }}</h2>
+  <button @click="msg = 'Jan'">Click Me</button>
+</template>
+
+<script>
+export default {
+  name: "Greet",
+  data() {
+    return {
+      msg: "Hello",
+    };
+  },
+};
+</script>
